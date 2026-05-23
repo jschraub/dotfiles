7fd64e90b6d844065abef8d2d10c8d04cbf64cfb
Jared Schraub <jaredschraub@outlook.com>
2026-05-23 02:06:35 -0500

    updated fish to use fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,9 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+# Set up fzf key bindings
+fzf --fish | source
+
+# Set up starship prompt
 eval "$(starship init fish)"
 
 # overwrite greeting
